a210b376ff1f77a73a01df4a94d5f46aa3a4afd2
Paxden <paxdenco@gmail.com>
2026-05-09 20:03:53 +0100

    cors

--- server.js
+++ server.js
@@ -49,7 +49,9 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:3000",
+  "https://hrorbn-analytics.vercel.app",
   "https://hrorbn-analytics.vercel.app/",
+  "https://nmcnanalysis-client.vercel.app",
 ];
 
 app.use(
